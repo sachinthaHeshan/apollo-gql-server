e2086e19a38f2eed54bab98a3a021a2a396a320a
sachinthaHeshan <sachinthazc@gmail.com>
2022-12-06 00:27:21 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+
+  const resolvers = {
+    Query: {
+      books: () => books,
+      hello:()=> 'Hello Bro!'
+    },
+  };
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `
+  type Book {
+    title: String
+    author: String
+  } 
+  
+  type Book {
+    title: String
+    author: String
+  } 
+
+  type Query {
+    books: [Book]
+    hello: String
+    countries: 
+  }
+ 
+`;
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  
+  // Passing an ApolloServer instance to the `startStandaloneServer` function:
+  //  1. creates an Express app
+  //  2. installs your ApolloServer instance as middleware
+  //  3. prepares your app to handle incoming requests
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+  
+  console.log(`🚀  Server ready at: ${url}`);
